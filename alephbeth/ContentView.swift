6be257a376d31e8315674b30bad092121846a33d
tomita28 <d.midoh28@gmail.com>
2020-05-01 14:19:58 +0900

    Initial Commit

---
+++ alephbeth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alephbeth
+//
+//  Created by 御堂 大嗣 on 2020/05/01.
+//  Copyright © 2020 御堂 大嗣. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
